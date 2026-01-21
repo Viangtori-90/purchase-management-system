8c06f4280ce15aa92d583ad555786d4e59461ba6
Viangtori-90 <tori.uk356@gmail.com>
2026-01-22 02:49:57 +0300

    Delete src directory

--- src/main/java/com/example/purchaseapp/repository/LotRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.purchaseapp.repository;
-
-import com.example.purchaseapp.entity.Lot;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LotRepository extends JpaRepository<Lot, String> {
-}
